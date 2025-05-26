48e0bfd57dcc25f4860c8babc196633a2da334a1
Marksmanju <163834304+Marksmanju@users.noreply.github.com>
2025-05-26 14:38:26 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="cs">
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+  <meta charset="UTF-8">
+  <title>Online Casino</title>
+  <link rel="stylesheet" href="../global.css">
 </head>
 <body>
-    cus
+
+  <div class="layout">
+    <aside class="main_page-aside">
+      <?php require "../aside/index.php"  ?>
+    </aside>
+
+    <main class="main_page-main">
+      <section class="main_page-news">
+        <div class="main_page-welcome_box">
+          Hi "username" welcome to Sypáč
+        </div>
+      </section>
+
+      <section class="main_page-bonus">
+        <div class="main_page-bonus_box">
+
+        </div>
+      </section>
+
+      <section class="main_page-games">
+        <div class="game_box">hra</div>
+        <div class="game_box">hra</div>
+        <div class="game_box">hra</div>
+        <div class="game_box">hra</div>
+      </section>
+    </main>
+  </div>
+
+  <?php require "../footer/index.php"?>
+
+  <script src="/script.js"></script>
 </body>
 </html>
