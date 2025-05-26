f0605887f46b1ba7af58b9f0ef983e66deae2545
DominikHOKR <hokr.d@ssmg.cz>
2025-05-26 14:13:35 +0200

    Add files via upload

---
+++ database.php
@@ -0,0 +1,45 @@
+<?php
+class DB {
+    private $pdo;
+
+    // DATABASE CONNECTION
+    public function __construct() {
+        $host = 'localhost';
+        $dbname = 'hokr_casino_db';
+        $user = 'hokr';
+        $pass = 'kWg2kS99mAGXLUGX';
+
+        try {
+            $this->pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $user, $pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Database ERROR: " . $e->getMessage());
+        }
+    }
+
+    // INSERT, UPDATE, DELETE
+    public function run($sql, $values = []) {
+        $stmt = $this->pdo->prepare($sql);
+        return $stmt->execute($values);
+    }
+
+    // SELECT
+    public function get($sql, $values = []) {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($values);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    // LOG
+    public function log($user_id, $action, $detail) {
+        $sql = "INSERT INTO logs VALUES (NULL, ?, ?, ?, NOW())";
+        $this->run($sql, [$user_id, $action, $detail]);
+    }
+
+    public function getOne($sql, $values = []) {
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->execute($values);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+}
+?>
