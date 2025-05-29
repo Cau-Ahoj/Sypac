4a939041e84c80ec09125557a72a8b7392b39eaf
Marksmanju <163834304+Marksmanju@users.noreply.github.com>
2025-05-29 14:05:19 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//Kód pro vypnutí a zapnutí okna s pravidly
 function togglePopup() {
   const popup = document.getElementById("popup");
   const isHidden = window.getComputedStyle(popup).display === "none";
